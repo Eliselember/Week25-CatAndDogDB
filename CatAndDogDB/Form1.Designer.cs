a8f939c63571301a354d452105090fa9fd17ee71
Opilane TTHK <opilane@tthk.ee>
2022-03-03 12:33:28 +0200

    week25

--- CatAndDogDB/Form1.Designer.cs
+++ CatAndDogDB/Form1.Designer.cs
@@ -28,13 +28,71 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.listPets = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listPetNames = new System.Windows.Forms.Label();
+            this.listPetName = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // listPets
+            // 
+            this.listPets.FormattingEnabled = true;
+            this.listPets.Location = new System.Drawing.Point(39, 115);
+            this.listPets.Name = "listPets";
+            this.listPets.Size = new System.Drawing.Size(120, 95);
+            this.listPets.TabIndex = 0;
+            this.listPets.SelectedIndexChanged += new System.EventHandler(this.listPets_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Pets";
+            // 
+            // listPetNames
+            // 
+            this.listPetNames.AutoSize = true;
+            this.listPetNames.Location = new System.Drawing.Point(442, 44);
+            this.listPetNames.Name = "listPetNames";
+            this.listPetNames.Size = new System.Drawing.Size(59, 13);
+            this.listPetNames.TabIndex = 2;
+            this.listPetNames.Text = "Pet Names";
+            this.listPetNames.Click += new System.EventHandler(this.listPets_SelectedIndexChanged);
+            // 
+            // listPetName
+            // 
+            this.listPetName.FormattingEnabled = true;
+            this.listPetName.Location = new System.Drawing.Point(445, 115);
+            this.listPetName.Name = "listPetName";
+            this.listPetName.Size = new System.Drawing.Size(120, 95);
+            this.listPetName.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listPetName);
+            this.Controls.Add(this.listPetNames);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listPets);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listPets;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label listPetNames;
+        private System.Windows.Forms.ListBox listPetName;
     }
 }
 
--- CatAndDogDB/Form1.cs
+++ CatAndDogDB/Form1.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +14,52 @@ namespace CatAndDogDB
 {
     public partial class Form1 : Form
     {
+        string connectionString;
+        SqlConnection connection;
         public Form1()
         {
             InitializeComponent();
+            connectionString = ConfigurationManager.ConnectionStrings["CatAndDogDB.Properties.Settings.PetsConnectionString"].ConnectionString;
+        }
+
+        private void PopulatePetsTable()
+        {
+            using (connection = new SqlConnection(connectionString))
+            using (SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM PetType", connection))
+            {
+                DataTable petTable = new DataTable();
+                adapter.Fill(petTable);
+
+                listPets.DisplayMember = "PetTypeName";
+                listPets.ValueMember = "Id";
+                listPets.DataSource = petTable;
+            }
+        }
+        private void PopulatePetName()
+        {
+            string query = "SELECT Pet.Name FROM PetType INNER JOIN Pet ON Pet.TypeId = PetType.Id WHERE PetType.Id = @TypeId;";
+            using (connection = new SqlConnection(connectionString))
+            using (SqlCommand command = new SqlCommand(query, connection))
+            using (SqlDataAdapter adapter = new SqlDataAdapter(command))
+            {
+              command.Parameters.AddWithValue("@TypeId", listPets.SelectedValue);
+                DataTable petNameTable = new DataTable();
+                adapter.Fill(petNameTable);
+
+                listPetName.DisplayMember = "Name";
+                listPetName.ValueMember = "Id";
+                listPetName.DataSource = petNameTable;
+            }
+        }
+
+        private void listPets_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            PopulatePetName();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            PopulatePetsTable();
         }
     }
 }
